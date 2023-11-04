b12f8a453e03f45e96c26573ebaba67a3af8241a
187525 <187525@ust-global.com>
2023-11-04 23:37:07 +0530

    Commit 4-11-2023

---
+++ CaseStudy/MyException/MyProductException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy.MyException
+{
+    internal class MyProductException
+    {
+
+        public static Dictionary<string, string> exMessageList = new Dictionary<string, string>()
+        {
+            { "One","ProductId is not null!!!" },
+            { "Two","Quantity not zero"},
+            {"Three", "Product Cost is greater than zero" },
+            {"Five","ProductName Not Null" }
+
+
+
+        };
+        internal class UserException : Exception
+        {
+            public UserException(string message) : base(message)
+            {
+
+            }
+        }
+    }
+}
